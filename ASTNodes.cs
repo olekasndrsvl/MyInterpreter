8510a9025e4954ac7621da7b85e483ac99f1550a
Лужняк Александр <Lyzhniak2005@yandex.ru>
2025-08-07 00:15:48 +0300

    ParserBase

---
+++ ASTNodes.cs
@@ -0,0 +1,322 @@
+﻿namespace MyInterpreter;
+
+public class ASTNodes
+{
+    public class Node
+    {
+        public virtual void Visit<T>(IVisitor<T> visitor)
+        {
+            visitor.VisitNode(this);
+        }
+
+        public virtual void Visit(IVisitorP visitor)
+        {
+            visitor.VisitNode(this);
+        }
+    }
+   
+    public class ExprNode : Node
+    {
+        public override void Visit<T>(IVisitor<T> visitor)
+        {
+            visitor.VisitExprNode(this);
+        }
+
+        public override void Visit(IVisitorP visitor)
+        {
+            visitor.VisitExprNode(this);
+        }
+    }
+    
+    public class StatementNode : Node
+    {
+        public override void Visit<T>(IVisitor<T> visitor)
+        {
+            visitor.VisitStatementNode(this);
+        }
+
+        public override void Visit(IVisitorP visitor)
+        {
+            visitor.VisitStatementNode(this);
+        }
+    }
+    public class BinOpNode :ExprNode
+    {
+        public ExprNode Left { get; set; }
+        public ExprNode Right { get; set; }
+        public string Operator { get; set; }
+
+        public BinOpNode(ExprNode left, ExprNode right, string op)
+        {
+            Left = left;
+            Right = right;
+            Operator = op;
+        }
+        public override void Visit<T>(IVisitor<T> visitor)
+        {
+            visitor.VisitBinOp(this);
+        }
+
+        public override void Visit(IVisitorP visitor)
+        {
+            visitor.VisitBinOp(this);
+        }
+    } 
+    public class ExprListNode: ExprNode
+    {
+        public List<ExprNode> lst = new List<ExprNode>();
+        public void Add(ExprNode statement)
+        {
+            lst.Add(statement);
+        }
+        public override void Visit<T>(IVisitor<T> visitor)
+        {
+            visitor.VisitExprList(this);
+        }
+
+        public override void Visit(IVisitorP visitor)
+        {
+            visitor.VisitExprList(this);
+        }
+    }
+    public class StatementListNode:StatementNode
+    {
+        public List<StatementNode> lst = new List<StatementNode>();
+
+        public void Add(StatementNode statement)
+        {
+            lst.Add(statement);
+        }
+        public override void Visit<T>(IVisitor<T> visitor)
+        {
+            visitor.VisitStatementList(this);
+        }
+
+        public override void Visit(IVisitorP visitor)
+        {
+            visitor.VisitStatementList(this);
+        }
+    }
+    public class IntNode :ExprNode
+    {
+        public int value;
+
+        public IntNode(int value)
+        {
+            this.value = value;
+        }
+        public override void Visit<T>(IVisitor<T> visitor)
+        {
+            visitor.VisitInt(this);
+        }
+
+        public override void Visit(IVisitorP visitor)
+        {
+            visitor.VisitInt(this);
+        }
+    }
+    public class DoubleNode : ExprNode
+    {
+        public double value;
+
+        public DoubleNode(double value)
+        {
+            this.value = value;
+        }
+        public override void Visit<T>(IVisitor<T> visitor)
+        {
+            visitor.VisitDouble(this);
+        }
+
+        public override void Visit(IVisitorP visitor)
+        {
+            visitor.VisitDouble(this);
+        }
+    }
+    public class IdNode:ExprNode
+    {
+        public string Name { get; set; }
+
+        public IdNode(string name)
+        {
+            Name = name;
+        }
+        public override void Visit<T>(IVisitor<T> visitor)
+        {
+            visitor.VisitId(this);
+        }
+
+        public override void Visit(IVisitorP visitor)
+        {
+            visitor.VisitId(this);
+        }
+    }
+  
+    public class AssignNode :StatementNode
+    {
+        public IdNode Ident { get; set; }
+        public ExprNode Expr { get; set; }
+
+        public AssignNode(IdNode ident, ExprNode expr)
+        {
+            Ident = ident;
+            Expr = expr;
+        }
+        
+    }
+    public class IfNode : StatementNode
+    {
+        public ExprNode Condition { get; set; }
+        public StatementNode ThenStat { get; set; }
+        public StatementNode ElseStat { get; set; }
+
+        public IfNode(ExprNode condition, StatementNode thenStat, StatementNode elseStat)
+        {
+            Condition = condition;
+            ThenStat = thenStat;
+            ElseStat = elseStat;
+        }
+        public override void Visit<T>(IVisitor<T> visitor)
+        {
+            visitor.VisitIf(this);
+        }
+
+        public override void Visit(IVisitorP visitor)
+        {
+            visitor.VisitIf(this);
+        }
+    }
+    public class WhileNode :StatementNode
+    {
+        public ExprNode Condition { get; set; }
+        public StatementNode Stat { get; set; }
+
+        public WhileNode(ExprNode condition, StatementNode stat)
+        {
+            Condition = condition;
+            Stat = stat;
+        }
+        public override void Visit<T>(IVisitor<T> visitor)
+        {
+            visitor.VisitWhile(this);
+        }
+
+        public override void Visit(IVisitorP visitor)
+        {
+            visitor.VisitWhile(this);
+        }
+    }
+    public class ProcCallNode : StatementNode
+    { 
+        public IdNode Name { get; set; }
+        public ExprListNode Pars { get; set; }
+
+        public ProcCallNode(IdNode name, ExprListNode pars)
+        {
+            Name = name;
+            Pars = pars;
+        }
+        public override void Visit<T>(IVisitor<T> visitor)
+        {
+            visitor.VisitProcCall(this);
+        }
+
+        public override void Visit(IVisitorP visitor)
+        {
+            visitor.VisitProcCall(this);
+        }
+    }
+    public class FuncCallNode :StatementNode
+    {
+       public IdNode Name { get; set; }
+       public ExprListNode Pars { get; set; }
+
+       public FuncCallNode(IdNode name, ExprListNode pars)
+       {
+           Name = name;
+           Pars = pars;
+       }
+       public override void Visit<T>(IVisitor<T> visitor)
+       {
+           visitor.VisitFuncCall(this);
+       }
+
+       public override void Visit(IVisitorP visitor)
+       {
+           visitor.VisitFuncCall(this);
+       }
+    }
+   
+    public interface IVisitor<T>
+    {
+        T VisitNode(Node bin);
+        T VisitExprNode(ExprNode bin);
+        T VisitStatementNode(StatementNode bin);
+        T VisitBinOp(BinOpNode bin);
+        T VisitStatementList(StatementListNode stl);
+        T VisitExprList(ExprListNode exlist);
+        T VisitInt(IntNode n);
+        T VisitDouble(DoubleNode d);
+        T VisitId(IdNode id);
+        T VisitAssign(AssignNode ass);
+        T VisitIf(IfNode ifn);
+        T VisitWhile(WhileNode whn);
+        T VisitProcCall(ProcCallNode p);
+        T VisitFuncCall(FuncCallNode f);
+    }
+    public interface IVisitorP
+    {
+        void VisitNode(Node bin);
+        void VisitExprNode(ExprNode bin);
+        void VisitStatementNode(StatementNode bin);
+        void VisitBinOp(BinOpNode bin);
+        void VisitStatementList(StatementListNode stl);
+        void VisitExprList(ExprListNode exlist);
+        void VisitInt(IntNode n);
+        void VisitDouble(DoubleNode d);
+        void VisitId(IdNode id);
+        void VisitAssign(AssignNode ass);
+        void VisitIf(IfNode ifn);
+        void VisitWhile(WhileNode whn);
+        void VisitProcCall(ProcCallNode p);
+        void VisitFuncCall(FuncCallNode f);
+    }
+    public static class ASTBuilder
+    {
+       
+        public static BinOpNode Bin(ExprNode left, string op, ExprNode right) 
+            => new BinOpNode(left, right, op);
+    
+      
+        public static AssignNode Ass(IdNode ident, ExprNode expr) 
+            => new AssignNode(ident, expr);
+    
+       
+        public static IdNode Id(string name) 
+            => new IdNode(name);
+    
+     
+        public static DoubleNode Num(double value) 
+            => new DoubleNode(value);
+    
+       
+        public static IfNode Iff(ExprNode cond, StatementNode thenBranch, StatementNode elseBranch) 
+            => new IfNode(cond, thenBranch, elseBranch);
+    
+        
+        public static WhileNode Wh(ExprNode cond, StatementNode body) 
+            => new WhileNode(cond, body);
+    
+      
+        public static StatementListNode StL(params StatementNode[] statements)
+            => new StatementListNode{ lst = statements.ToList()};
+    
+       
+        public static ExprListNode ExL(params ExprNode[] expressions)
+            => new ExprListNode {  lst = expressions.ToList() };
+    
+       
+        public static ProcCallNode ProcCall(IdNode name, ExprListNode args)
+            => new ProcCallNode(name, args);
+    }
+}
---
+++ Parser.cs
@@ -0,0 +1,143 @@
+﻿using System.Text;
+
+namespace MyInterpreter;
+
+public class ParserBase
+{
+    private Lexer lex;
+    protected int current = 0;
+    protected Token curToken;
+
+    public ParserBase(Lexer lex)
+    {
+        this.lex = lex;
+        NextLexem();
+    }
+    /// Вернуть текущий токен и перейти к следующему
+    public Token NextLexem()
+    {
+        var result = curToken;
+        curToken = lex.NextToken();
+        if(!IsAtEnd())
+            current++;
+        return result;
+    }
+
+    /// Проверить, что тип текушего токена совпадает с одним из данных типов
+    public bool At(params TokenType[] types)
+    {
+        return types.Any(t => PeekToken().type == t);
+    }
+    
+    public void Check(params TokenType[] types)
+    {
+        if (!At(types))
+        {
+            ExpectedError(types);
+        }
+    }
+
+    /// Проверить, что тип текушего токена совпадает с одним из данных типов, 
+    /// и в случае успеха перейти к следующему символу
+    public bool IsMatch(params TokenType[] types)
+    {
+        var res = At(types);
+        if (res)
+            NextLexem();
+        return res;
+    }
+
+    /// Проверить на соответствие и вернуть токен или выбросить ошибку
+    /// В отличие от At в случае неуспеха бросает ошибку
+    public Token Requires(params TokenType[] types)
+    {
+        if (At(types))
+        {
+            return NextLexem();
+        }
+        else
+        {
+            ExpectedError(types);
+        }
+        return null;
+    }
+    public Token PeekToken()
+    {
+        return curToken;
+    }
+
+    public Token CurrentToken()
+    {
+        return curToken;
+    }
+    public bool IsAtEnd()
+    {
+        return PeekToken().type == TokenType.Eof;
+    }
+
+    public void ExpectedError(params TokenType[] types)
+    {
+        var sb = new StringBuilder();
+        foreach (var type in types)
+        {
+            sb.Append(type.ToString());
+            sb.Append("или");
+        }
+        
+        throw new MyInterpreter.ComplierExceptions.SyntaxException($"{sb.ToString()} ожидалось, но {PeekToken().type} найдено",
+            PeekToken().pos);
+    }
+}
+
+
+// Program := StatementList
+// StatementList := Statement (';' Statement)*
+// Statement := Assign | ProcCall | IfStatement | WhileStatement | BlockStatement 
+// Assign := Id ('=' | '+=' | '-=' | '*=' | '/=') Expr 
+// ProcCall := Id '(' ExprList ')
+// FuncCall := Id '(' ExprList ')
+// WhileStatement := while Expr do Statement
+// IfStatement := if Expr then Statement [else Statement]
+// BlockStatement := '{' StatementList '}' 
+// Expr := Comp (CompOp Comp)*
+// CompOp := '<' | '>' | '<=' | '>=' | '==' | '!='
+// Comp := Term (AddOp Term)*
+// AddOp := '+' | '-' | '||'
+// Term := Factor (MultOp Factor)*
+// MultOp := '*' | '/' | '&&'
+// Factor := IntNum | DoubleNum | FuncCall | '(' Expr ') 
+// ExprList := Expr (',' Expr)*
+
+public class Parser: ParserBase
+{
+    public Parser(Lexer lex) : base(lex)
+    {
+    }
+    
+    // Program := StatementList  
+    public ASTNodes.StatementNode MainProgram()
+    {
+        current = 0;
+        var result = StatementList();
+        Requires(TokenType.Eof);
+        return result;
+    }
+    
+    // StatementList := Statement (';' Statement)*
+    public ASTNodes.StatementNode StatementList()
+    {
+        var stl = new ASTNodes.StatementListNode();
+        stl.Add(Statement());
+        while (IsMatch(TokenType.Semicolon))
+        {
+            stl.Add(Statement());
+        }
+        return stl;
+    }
+    
+    // Statement := Assign | ProcCall | IfStatement | WhileStatement | BlockStatement 
+    public ASTNodes.StatementNode Statement()
+    {
+        return null;
+    }
+}
