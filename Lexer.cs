1b0e9c8e5ef479d4e1d78f93435f910fc0c15f6f
Alexander Luzhniak <Lyzhniak2005@yandex.ru>
2026-02-28 16:50:29 +0300

    Added support for negative numbers

--- Lexer.cs
+++ Lexer.cs
@@ -198,162 +198,287 @@ public class Token : TokenBase
 
 
 public partial class Lexer : LexerBase<TokenType>
+{
+    private static readonly Dictionary<string, TokenType> Keywords = new Dictionary<string, TokenType>
     {
-        private static readonly Dictionary<string, TokenType> Keywords = new Dictionary<string, TokenType>
+        ["True"] = TokenType.tkTrue,
+        ["False"] = TokenType.tkFalse,
+        ["if"] = TokenType.tkIf,
+        ["then"] = TokenType.tkThen,
+        ["else"] = TokenType.tkElse,
+        ["while"] = TokenType.tkWhile,
+        ["do"] = TokenType.tkDo,
+        ["for"] = TokenType.tkFor,
+        ["def"] = TokenType.tkDef,
+        ["return"] = TokenType.tkReturn,
+        ["var"] = TokenType.tkVar,
+    };
+
+    private TokenT<TokenType> previousToken = null;
+
+    public Lexer(string code) : base(code) { }
+
+    public override bool TokenTypeIsEof(TokenType tt) => tt == TokenType.Eof;
+
+    private TokenT<TokenType> ScanIdentifier(int startPos)
+    {
+        var identifier = ReadIdentifier();
+        var tokenType = Keywords.TryGetValue(identifier, out var keywordType) 
+            ? keywordType 
+            : TokenType.Id;
+        return new TokenT<TokenType>(tokenType, startPos, identifier);
+    }
+
+    private TokenT<TokenType> ScanString(int startPos)
+    {
+        var strWithQuotes = ReadString('"');
+        var strValue = strWithQuotes.Substring(1, strWithQuotes.Length - 2);
+        return new TokenT<TokenType>(TokenType.StringLiteral, startPos, strValue);
+    }
+     
+    private TokenT<TokenType> ScanNumber(int startPos)
+    {
+        var numberStr = ReadNumber();
+        
+        if (string.IsNullOrEmpty(numberStr))
         {
-            ["True"] = TokenType.tkTrue,
-            ["False"] = TokenType.tkFalse,
-            ["if"] = TokenType.tkIf,
-            ["then"] = TokenType.tkThen,
-            ["else"] = TokenType.tkElse,
-            ["while"] = TokenType.tkWhile,
-            ["do"] = TokenType.tkDo,
-            ["for"] = TokenType.tkFor,
-            ["def"] = TokenType.tkDef,
-            ["return"] = TokenType.tkReturn,
-            ["var"] =TokenType.tkVar,
-        };
-
-        public Lexer(string code) : base(code) { }
-
-        public override bool TokenTypeIsEof(TokenType tt) => tt == TokenType.Eof;
-
-        private TokenT<TokenType> ScanIdentifier(int startPos)
-        {
-            var identifier = ReadIdentifier();
-            var tokenType = Keywords.TryGetValue(identifier, out var keywordType) 
-                ? keywordType 
-                : TokenType.Id;
-            return new TokenT<TokenType>(tokenType, startPos, identifier);
+            CompilerExceptions.LexerError("Ожидается число", CurrentPosition());
+            return null;
         }
-
-        private TokenT<TokenType> ScanString(int startPos)
+        
+        try
         {
-            var strWithQuotes = ReadString('"');
-            var strValue = strWithQuotes.Substring(1, strWithQuotes.Length - 2);
-            return new TokenT<TokenType>(TokenType.StringLiteral, startPos, strValue);
-        }
-         
-        private TokenT<TokenType> ScanNumber(int startPos)
-        {
-            var numberStr = ReadNumber();
-            
             if (numberStr.Contains("."))
-                return new TokenT<TokenType>(TokenType.DoubleLiteral, startPos, double.Parse(numberStr, CultureInfo.InvariantCulture));
-            else
-                return new TokenT<TokenType>(TokenType.Int, startPos, int.Parse(numberStr));
-        }
-
-        private TokenT<TokenType> ScanSymbol(int startPos)
-        {
-            StartToken();
-            var c = NextChar();
-            
-            switch (c)
             {
-                case ',':
-                    return new TokenT<TokenType>(TokenType.Comma, startPos, ",");
-                case ')':
-                    return new TokenT<TokenType>(TokenType.RPar, startPos, ")");
-                case '(':
-                    return new TokenT<TokenType>(TokenType.LPar, startPos, "(");
-                case '}':
-                    return new TokenT<TokenType>(TokenType.RBrace, startPos, "}");
-                case '{':
-                    return new TokenT<TokenType>(TokenType.LBrace, startPos, "{");
-                case ';':
-                    return new TokenT<TokenType>(TokenType.Semicolon, startPos, ";");
-                case '.':
-                    return new TokenT<TokenType>(TokenType.Dot, startPos, ".");
-                    
-                case '+':
-                    return new TokenT<TokenType>(
-                        IsMatch('=') ? TokenType.AssignPlus : TokenType.Plus,
-                        startPos, GetTokenText());
-                        
-                case '-':
-                    return new TokenT<TokenType>(
-                        IsMatch('=') ? TokenType.AssignMinus : TokenType.Minus,
-                        startPos, GetTokenText());
-                        
-                case '*':
-                    return new TokenT<TokenType>(
-                        IsMatch('=') ? TokenType.AssignMult : TokenType.Multiply,
-                        startPos, GetTokenText());
-                        
-                case '/':
-                    if (IsMatch('/'))
-                    {
-                        // Skip comment
-                        while (PeekChar() != '\n' && !IsAtEnd())
-                            NextChar();
-                        return NextToken(); // Recursively get next token
-                    }
-                    else
-                    {
-                        return new TokenT<TokenType>(
-                            IsMatch('=') ? TokenType.AssignDiv : TokenType.Divide,
-                            startPos, GetTokenText());
-                    }
-                    
-                case '!':
-                    return new TokenT<TokenType>(
-                        IsMatch('=') ? TokenType.NotEqual : TokenType.tkNot,
-                        startPos, GetTokenText());
-                        
-                case '=':
-                    return new TokenT<TokenType>(
-                        IsMatch('=') ? TokenType.Equal : TokenType.Assign,
-                        startPos, GetTokenText());
-                        
-                case '>':
-                    return new TokenT<TokenType>(
-                        IsMatch('=') ? TokenType.GreaterEqual : TokenType.Greater,
-                        startPos, GetTokenText());
-                        
-                case '<':
-                    return new TokenT<TokenType>(
-                        IsMatch('=') ? TokenType.LessEqual : TokenType.Less,
-                        startPos, GetTokenText());
-                        
-                case '&':
-                    if (IsMatch('&'))
-                        return new TokenT<TokenType>(TokenType.tkAnd, startPos, "&&");
-                    else
-                        MyInterpreter.CompilerExceptions.LexerError("Ожидается &&", CurrentPosition());
-                    break;
-                    
-                case '|':
-                    if (IsMatch('|'))
-                        return new TokenT<TokenType>(TokenType.tkOr, startPos, "||");
-                    else
-                        MyInterpreter.CompilerExceptions.LexerError("Ожидается ||", CurrentPosition());
-                    break;
-                    
-                default:
-                    MyInterpreter.CompilerExceptions.LexerError($"Неизвестный символ: {c}", new Position(line, startPos));
-                    break;
+                double value = double.Parse(numberStr, CultureInfo.InvariantCulture);
+                return new TokenT<TokenType>(TokenType.DoubleLiteral, startPos, value);
             }
-            
-            return null; // Will never be reached due to exceptions
-        }
-
-        public override TokenT<TokenType> NextToken()
-        {
-            SkipWhitespace();
-            var pos = CurrentPosition();
-            if (IsAtEnd())
-                return new TokenT<TokenType>(TokenType.Eof, pos.Column, "Eof");
-            
-            var c = PeekChar();
-
-            if (char.IsDigit(c))
-                return ScanNumber(pos.Column);
-            else if (IsAlpha(c))
-                return ScanIdentifier(pos.Column);
-            else if (c == '"')
-                return ScanString(pos.Column);
             else
-                return ScanSymbol(pos.Column);
+            {
+                int value = int.Parse(numberStr, CultureInfo.InvariantCulture);
+                return new TokenT<TokenType>(TokenType.Int, startPos, value);
+            }
+        }
+        catch (FormatException)
+        {
+            CompilerExceptions.LexerError($"Некорректный формат числа: {numberStr}", CurrentPosition());
+            return null;
+        }
+        catch (OverflowException)
+        {
+            CompilerExceptions.LexerError($"Число {numberStr} выходит за пределы допустимого диапазона", CurrentPosition());
+            return null;
         }
     }
+
+    // Проверяет, ожидается ли начало выражения (т.е. минус может быть унарным)
+    private bool IsStartOfExpressionContext()
+    {
+        if (previousToken == null)
+            return true; // Начало программы
+        
+        var prevType = previousToken.Typ;
+        
+        // Минус может быть унарным, если предыдущий токен - это:
+        return prevType == TokenType.LPar ||           // Открывающая скобка: (-5)
+               prevType == TokenType.Comma ||          // Запятая: func(1, -5)
+               prevType == TokenType.Assign ||         // Присваивание: x = -5
+               prevType == TokenType.AssignPlus ||     // Составное присваивание: x += -5
+               prevType == TokenType.AssignMinus ||   
+               prevType == TokenType.AssignMult ||
+               prevType == TokenType.AssignDiv ||
+               prevType == TokenType.Plus ||           // Бинарные операторы: a + -5
+               prevType == TokenType.Minus ||          // a - -5 (второй минус - унарный)
+               prevType == TokenType.Multiply ||       // a * -5
+               prevType == TokenType.Divide ||         // a / -5
+               prevType == TokenType.tkAnd ||          // Логические операторы: true && -5
+               prevType == TokenType.tkOr ||           // false || -5
+               prevType == TokenType.tkNot ||          // not -5
+               prevType == TokenType.Less ||           // Операторы сравнения: a < -5
+               prevType == TokenType.LessEqual ||      // a <= -5
+               prevType == TokenType.Greater ||        // a > -5
+               prevType == TokenType.GreaterEqual ||   // a >= -5
+               prevType == TokenType.Equal ||          // a == -5
+               prevType == TokenType.NotEqual ||       // a != -5
+               prevType == TokenType.tkReturn ||       // Ключевые слова: return -5
+               prevType == TokenType.tkIf ||           // if -5
+               prevType == TokenType.tkThen ||         // then -5
+               prevType == TokenType.tkElse ||         // else -5
+               prevType == TokenType.tkWhile ||        // while -5
+               prevType == TokenType.tkDo ||           // do -5
+               prevType == TokenType.tkFor ||          // for -5
+               prevType == TokenType.tkDef ||          // def -5
+               prevType == TokenType.tkVar;            // var -5
+    }
+
+    private TokenT<TokenType> ScanSymbol(int startPos)
+    {
+        StartToken();
+        var c = NextChar();
+        TokenT<TokenType> result = null;
+        
+        switch (c)
+        {
+            case ',':
+                result = new TokenT<TokenType>(TokenType.Comma, startPos, ",");
+                break;
+            case ')':
+                result = new TokenT<TokenType>(TokenType.RPar, startPos, ")");
+                break;
+            case '(':
+                result = new TokenT<TokenType>(TokenType.LPar, startPos, "(");
+                break;
+            case '}':
+                result = new TokenT<TokenType>(TokenType.RBrace, startPos, "}");
+                break;
+            case '{':
+                result = new TokenT<TokenType>(TokenType.LBrace, startPos, "{");
+                break;
+            case ';':
+                result = new TokenT<TokenType>(TokenType.Semicolon, startPos, ";");
+                break;
+            case '.':
+                result = new TokenT<TokenType>(TokenType.Dot, startPos, ".");
+                break;
+                
+            case '+':
+                result = new TokenT<TokenType>(
+                    IsMatch('=') ? TokenType.AssignPlus : TokenType.Plus,
+                    startPos, GetTokenText());
+                break;
+                
+            case '-':
+                // Проверяем, является ли это составным оператором присваивания
+                if (IsMatch('='))
+                {
+                    result = new TokenT<TokenType>(TokenType.AssignMinus, startPos, "-=");
+                }
+                // Проверяем, может ли это быть унарным минусом
+                else if (IsStartOfExpressionContext())
+                {
+                    // Если после минуса идет цифра - это отрицательное число
+                    if (char.IsDigit(PeekChar()))
+                    {
+                        result = ScanNumber(startPos);
+                        // Меняем знак числа на отрицательный
+                        if (result.Value is int intVal)
+                            result.Value = -intVal;
+                        else if (result.Value is double doubleVal)
+                            result.Value = -doubleVal;
+                    }
+                    else
+                    {
+                        // Иначе это унарный оператор минус (будет обработан парсером)
+                        result = new TokenT<TokenType>(TokenType.Minus, startPos, "-");
+                    }
+                }
+                else
+                {
+                    // Обычный бинарный оператор минус
+                    result = new TokenT<TokenType>(TokenType.Minus, startPos, "-");
+                }
+                break;
+                
+            case '*':
+                result = new TokenT<TokenType>(
+                    IsMatch('=') ? TokenType.AssignMult : TokenType.Multiply,
+                    startPos, GetTokenText());
+                break;
+                
+            case '/':
+                if (IsMatch('/'))
+                {
+                    while (PeekChar() != '\n' && !IsAtEnd())
+                        NextChar();
+                    return NextToken();
+                }
+                else
+                {
+                    result = new TokenT<TokenType>(
+                        IsMatch('=') ? TokenType.AssignDiv : TokenType.Divide,
+                        startPos, GetTokenText());
+                }
+                break;
+                
+            case '!':
+                result = new TokenT<TokenType>(
+                    IsMatch('=') ? TokenType.NotEqual : TokenType.tkNot,
+                    startPos, GetTokenText());
+                break;
+                
+            case '=':
+                result = new TokenT<TokenType>(
+                    IsMatch('=') ? TokenType.Equal : TokenType.Assign,
+                    startPos, GetTokenText());
+                break;
+                
+            case '>':
+                result = new TokenT<TokenType>(
+                    IsMatch('=') ? TokenType.GreaterEqual : TokenType.Greater,
+                    startPos, GetTokenText());
+                break;
+                
+            case '<':
+                result = new TokenT<TokenType>(
+                    IsMatch('=') ? TokenType.LessEqual : TokenType.Less,
+                    startPos, GetTokenText());
+                break;
+                
+            case '&':
+                if (IsMatch('&'))
+                    result = new TokenT<TokenType>(TokenType.tkAnd, startPos, "&&");
+                else
+                    CompilerExceptions.LexerError("Ожидается &&", CurrentPosition());
+                break;
+                
+            case '|':
+                if (IsMatch('|'))
+                    result = new TokenT<TokenType>(TokenType.tkOr, startPos, "||");
+                else
+                    CompilerExceptions.LexerError("Ожидается ||", CurrentPosition());
+                break;
+                
+            default:
+                CompilerExceptions.LexerError($"Неизвестный символ: {c}", new Position(line, startPos));
+                break;
+        }
+        
+        return result;
+    }
+
+    public override TokenT<TokenType> NextToken()
+    {
+        SkipWhitespace();
+        var pos = CurrentPosition();
+        
+        if (IsAtEnd())
+        {
+            var eofToken = new TokenT<TokenType>(TokenType.Eof, pos.Column, "Eof");
+            previousToken = eofToken;
+            return eofToken;
+        }
+        
+        var c = PeekChar();
+        TokenT<TokenType> token = null;
+
+        if (char.IsDigit(c))
+        {
+            token = ScanNumber(pos.Column);
+        }
+        else if (IsAlpha(c))
+        {
+            token = ScanIdentifier(pos.Column);
+        }
+        else if (c == '"')
+        {
+            token = ScanString(pos.Column);
+        }
+        else
+        {
+            token = ScanSymbol(pos.Column);
+        }
+        
+        previousToken = token;
+        return token;
+    }
+}
